c3b53f83467f35466b1077ed8b57ff495cb40eb3
wOkEaF101 <67075726+wOkEaF101@users.noreply.github.com>
2022-02-05 23:54:33 -0700

    Create network.sh

---
+++ init/network.sh
@@ -0,0 +1,19 @@
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
+
+echo "apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - 192.168.1.240-192.168.1.250
+" > metallb-cm.yaml
+
+kubectl apply -f metallb-cm.yaml
