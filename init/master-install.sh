4a3f41c2ac7b5102cabf13b93f676e2c682cb4a7
wOkEaF101 <67075726+wOkEaF101@users.noreply.github.com>
2022-02-05 19:35:47 -0700

    Update master-install.sh

--- init/master-install.sh
+++ init/master-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+sudo -i
+swapoff -a
 
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
 # Setup required sysctl params, these persist across reboots.
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
@@ -16,27 +18,29 @@ net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
 # Apply sysctl params without reboot
-sudo sysctl --system
+sysctl --system
 
-sudo apt-get update && sudo apt-get install containerd
+apt-get update && sudo apt-get install containerd
 
-sudo mkdir -p /etc/containerd
+mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 
-sudo systemctl restart containerd
+systemctl restart containerd
 
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
-sudo kubeadm init
+kubeadm init
 
-sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
+su k8sadmin
+
+cp /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config
