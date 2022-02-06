352fa757fcfcbcbfbbb2c422e85225420c7fe746
wOkEaF101 <67075726+wOkEaF101@users.noreply.github.com>
2022-02-05 20:17:16 -0700

    Update master-install.sh

--- init/master-install.sh
+++ init/master-install.sh
@@ -37,7 +37,7 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-sudo kubeadm init
+sudo kubeadm init --pod-network-cidr=10.0.1.0/24
 
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config && sudo chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config
