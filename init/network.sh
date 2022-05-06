4b11aeb7000112f310226477e9ff165ef6697890
wOkEaF101 <67075726+wOkEaF101@users.noreply.github.com>
2022-05-06 12:43:52 -0600

    Update network.sh

--- init/network.sh
+++ init/network.sh
@@ -17,7 +17,3 @@ data:
 " > metallb-cm.yaml
 
 kubectl apply -f metallb-cm.yaml
-
-helm upgrade --install ingress-nginx ingress-nginx \
-  --repo https://kubernetes.github.io/ingress-nginx \
-  --namespace ingress-nginx --create-namespace
